872b2321d758884341fb724812bbddf7f9140a71
Roger Luis <rogerluisvicini21@hotmail.com>
2022-06-14 17:07:39 -0300

    ATT02

--- main.c
+++ main.c
@@ -6,21 +6,34 @@
 int main()
 {
     char PalavraUsuario[6], PalavraSorteada[6] = "CACAU";
-    int vida = 6, AUX = 0, *ptr, modo = 0;
+    int vida = 6, AUX = 0, *ptrL = NULL,*ptrC = NULL, modo = 0, TamL = 6, TamC = 5;
     char tabuleiro[6][5];
 
-    printf("---TERMO---\n\n");
+    do {
+        printf("----MODOS----\n");
+        printf("FACIL -> 1\n");
+        printf("MEDIO -> 2\n");
+        printf("DIFICIL -> 3\n");
+        printf("\nDigite o modo: ");
+        scanf("%d", &modo);
+        system("cls");
+    } while (modo < 1 || modo > 3);
 
-    for(int linha = 0; linha < 6; linha++){
-        for(int coluna = 0; coluna < 5; coluna++){
+    for(int linha = 0; linha < TamL; linha++){
+        for(int coluna = 0; coluna < TamC; coluna++){
             tabuleiro[linha][coluna] = 'x';
         }
     }
 
+    printf("---TERMO---\n");
+
     do{
         printf("VIDAS: %d\n",vida);
-        printf("Digite a palavra: ");
-        scanf("%s", PalavraUsuario);
+        //verifica se a palavra digitada tem 5 letras
+        do{
+            printf("Digite a palavra: ");
+            scanf("%s", PalavraUsuario);
+        } while (strlen(PalavraUsuario) < 5 || strlen(PalavraUsuario) > 5);
 
         //transforma a string que o usuario da em maiusculo
         for(int C = 0; C <= 5; C++){
@@ -28,27 +41,29 @@ int main()
         }
 
         //escreve o tabuleiro
-        for(int linha = 0; linha < 6; linha++){
-            for(int coluna = 0; coluna < 5; coluna++){
+        for(int linha = 0; linha < TamL; linha++){
+            for(int coluna = 0; coluna < TamC; coluna++){
                 if(linha == AUX){
                     tabuleiro[linha][coluna] = PalavraUsuario[coluna];
                 }
                 if(tabuleiro[linha][coluna] == 'x'){
                     tabuleiro[linha][coluna] = ' ';
                 }
-
             }
         }
 
-        for(int linha = 0; linha < 6; linha++){
-            for(int coluna = 0; coluna < 5; coluna++){
+        //verifica se a pessoa acertou ou errou o chute
+        for(int linha = 0; linha < TamL; linha++){
+            for(int coluna = 0; coluna < TamC; coluna++){
                 printf("[ %c ]", tabuleiro[linha][coluna]);
             }
             printf("\n");
         }
 
         if(strcmp(PalavraUsuario,PalavraSorteada) == 0){
-            printf("\nACERTOU\n");
+            system("cls");
+            printf("\nACERTOU!\n");
+            printf("A Palavra era %s\n", PalavraSorteada);
             break;
         } else {
             vida--;
